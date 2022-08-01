4e5fc56eecbb893dbd3f81acabe1e314ac2ab262
Orlando Jose Umanzor Zelaya <orlandojose729@gmail.com>
2022-08-01 17:28:45 -0600

    Do some advance on styling the queried data
    Add a slight delay when quering for a city
    so the loading screen can be shown.

--- src/displayController.js
+++ src/displayController.js
@@ -38,18 +38,19 @@ export async function renderHomePage() {
         let header = document.createElement('header');
         let logoContainer = document.createElement('div')
         logoContainer.classList.add('logo-default')
+        let logoIcon = document.createElement('img')
+        logoIcon.src = './head-icon.gif'
         let logo = document.createElement('h1')
         logo.classList.add('logo-text-default')
         logo.textContent = 'W·e·a·t·h·e·r'
         logo.addEventListener('click', renderHomePage)
+        logoContainer.appendChild(logoIcon)
         logoContainer.appendChild(logo)
 
         let inputContainer = document.createElement('form')
         inputContainer.addEventListener('submit', (e) => {
             e.preventDefault()
-            renderQueriedCity().catch((error) => {
-                console.log(error)
-            })
+            renderQueriedCity()
         })
         inputContainer.classList.add('input-container-home')
         let inputBox = document.createElement('input')
@@ -72,12 +73,13 @@ export async function renderHomePage() {
 
         mainContainer.appendChild(header)
         mainContainer.appendChild(cardsContainer)
-
+        
         clear()
 
         document.body.appendChild(mainContainer)
-    }catch(error){
+    } catch(error) {
         let errorPromise = await error
+        console.log(error)
         renderError(errorPromise.message.charAt(0).toUpperCase() + errorPromise.message.slice(1))
     }
 }
@@ -85,21 +87,29 @@ export async function renderHomePage() {
 
 export async function renderQueriedCity() {
     try {
+        let retrievedData = await queryClimate(document.querySelector('input').value)
         renderLoader('Retrieving Data...')
-        let retrievedData = await queryClimate(document.querySelector('input'))
+        
         let mainContainer = document.createElement('main');
         mainContainer.classList.add('main-container-home');
 
         let header = document.createElement('header');
         let logoContainer = document.createElement('div')
         logoContainer.classList.add('logo-default')
+        let logoIcon = document.createElement('img')
+        logoIcon.src = './head-icon.gif'
         let logo = document.createElement('h1')
         logo.classList.add('logo-text-default')
         logo.textContent = 'W·e·a·t·h·e·r'
         logo.addEventListener('click', renderHomePage)
+        logoContainer.appendChild(logoIcon)
         logoContainer.appendChild(logo)
 
-        let inputContainer = document.createElement('div')
+        let inputContainer = document.createElement('form')
+        inputContainer.addEventListener('submit', (e) => {
+            e.preventDefault()
+            renderQueriedCity()
+        })
         inputContainer.classList.add('input-container-home')
         let inputBox = document.createElement('input')
         inputBox.classList.add('input-home')
@@ -117,13 +127,107 @@ export async function renderQueriedCity() {
         inputContainer.appendChild(searchButton)
         header.appendChild(logoContainer)
         header.appendChild(inputContainer)
-        mainContainer.appendChild(header)
 
-        clear()
-        document.body.appendChild(mainContainer)
+        let queriedMainContainer = document.createElement('div')
+        queriedMainContainer.classList.add('main-container-queried')
+
+        // top header
+        let queriedHeader = document.createElement('div')
+        queriedHeader.classList.add('sub-header-queried')
+
+        let cityName = document.createElement('div')
+        cityName.classList.add('city-name-queried')
+        cityName.textContent = retrievedData.name
+        let cityCountry = document.createElement('span')
+        cityCountry.classList.add('city-country-queried')
+        cityCountry.textContent = retrievedData.sys.country
+        cityName.appendChild(cityCountry)
+
+        let cityTemp = document.createElement('div')
+        cityTemp.classList.add('city-temp-queried')
+        cityTemp.textContent = `${Math.round(retrievedData.main.temp - 275)}°`
+        let weatherIcon = document.createElement('img')
+        weatherIcon.src = `http://openweathermap.org/img/wn/${retrievedData.weather[0].icon}@2x.png`
+        cityTemp.appendChild(weatherIcon)
+
+        let cityWeatherDescription = document.createElement('div')
+        cityWeatherDescription.classList.add('city-weather-queried')
+        cityWeatherDescription.textContent = retrievedData.weather[0].description.charAt(0).toUpperCase() + retrievedData.weather[0].description.slice(1)
+
+        queriedHeader.appendChild(cityName)
+        queriedHeader.appendChild(cityTemp)
+        queriedHeader.appendChild(cityWeatherDescription)
+
+        // middle container
+        let queriedAdditionalContainer = document.createElement('div')
+        queriedAdditionalContainer.classList.add('middle-container-queried')
+        
+        let cityHumidity = document.createElement('div')
+        cityHumidity.classList.add('city-humidity-container-queried')
+        let cityHumidityIcon = document.createElement('img')
+        cityHumidityIcon.classList.add('city-humidity-icon-queried')
+        cityHumidityIcon.src = './svg/drop.png'
+        let cityHumidityMainText = document.createElement('div')
+        cityHumidityMainText.classList.add('city-humidity-mainText-queried')
+        cityHumidityMainText.textContent = 'Humidity'
+        let cityHumidityText = document.createElement('div')
+        cityHumidityText.classList.add('city-humidity-text-queried')
+        cityHumidityText.textContent = `${retrievedData.main.humidity}%`
+        cityHumidity.appendChild(cityHumidityIcon)
+        cityHumidity.appendChild(cityHumidityMainText)
+        cityHumidity.appendChild(cityHumidityText)
+
+        let cityClouds = document.createElement('div')
+        cityClouds.classList.add('city-clouds-container-queried')
+        let cityCloudsIcon = document.createElement('img')
+        cityCloudsIcon.classList.add('city-clouds-icon-queried')
+        cityCloudsIcon.src = './svg/cloud.png'
+        let cityCloudsMainText = document.createElement('div')
+        cityCloudsMainText.classList.add('city-clouds-mainText-queried')
+        cityCloudsMainText.textContent = 'Clouds'
+        let cityCloudsText = document.createElement('div')
+        cityCloudsText.classList.add('city-clouds-text-queried')
+        cityCloudsText.textContent = `${retrievedData.clouds.all}%`
+        cityClouds.appendChild(cityCloudsIcon)
+        cityClouds.appendChild(cityCloudsMainText)
+        cityClouds.appendChild(cityCloudsText)
+
+        let cityVisibility = document.createElement('div')
+        cityVisibility.classList.add('city-visibility-container-queried')
+        let cityVisibilityIcon = document.createElement('img')
+        cityVisibilityIcon.classList.add('city-visibility-icon-queried')
+        cityVisibilityIcon.src = './svg/visibility.png'
+        let cityVisibilityMainText = document.createElement('div')
+        cityVisibilityMainText.classList.add('city-visibility-mainText-queried')
+        cityVisibilityMainText.textContent = 'Visibility'
+        let cityVisibilityText = document.createElement('div')
+        cityVisibilityText.classList.add('city-visibility-text-queried')
+        cityVisibilityText.textContent = `${(retrievedData.visibility / 1000).toFixed(2)} km`
+        cityVisibility.appendChild(cityVisibilityIcon)
+        cityVisibility.appendChild(cityVisibilityMainText)
+        cityVisibility.appendChild(cityVisibilityText)
+
+        queriedAdditionalContainer.appendChild(cityHumidity)
+        queriedAdditionalContainer.appendChild(cityClouds)
+        queriedAdditionalContainer.appendChild(cityVisibility)
+
+
+        // last container
+        let queriedAdditionalForecastContainer = document.createElement('div')
+
+        queriedMainContainer.appendChild(queriedHeader)
+        queriedMainContainer.appendChild(queriedAdditionalContainer)
+        queriedMainContainer.appendChild(queriedAdditionalForecastContainer)
+
+        mainContainer.appendChild(header)
+        mainContainer.appendChild(queriedMainContainer)
+        setTimeout(()=>{
+            clear()
+            document.body.appendChild(mainContainer)
+        },500)
+
     } catch (error) {
-        let errorPromise = await error
-        renderError(errorPromise.message.charAt(0).toUpperCase() + errorPromise.message.slice(1))
+        renderError(error.message.charAt(0).toUpperCase() + error.message.slice(1))
     }
 }
 
@@ -148,7 +252,7 @@ function renderError(text) {
     errorScreen.appendChild(errorText)
     document.body.appendChild(errorScreen)
 
-    setTimeout(renderHomePage, 4000)
+    setTimeout(renderHomePage, 3000)
 }
 
 function renderLoader(text) {
--- src/query.js
+++ src/query.js
@@ -2,7 +2,7 @@ export default async function queryClimate(location) {
     try {
         let response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=14680de0af166b3e9cfe19b841f217ef`, { mode: 'cors' })
         if(!response.ok){
-            throw(response.json())
+            throw(await response.json())
         }
 
         let retrievedData = await response.json()
