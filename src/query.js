9ae661e1de3a4c9c948871367ef7f51f0ac54aa8
Orlando Umanzor <orlandojose729@gmail.com>
2023-07-16 14:40:05 -0600

    Fix bug

--- src/query.js
+++ src/query.js
@@ -2,7 +2,7 @@ const apiKey = process.env.API_KEY;
 
 export async function queryClimate(location) {
     try {
-        let response = await fetch(`http://api.openweathermap.org/data/3.0/weather?q=${location}&appid=9fb86b3bf0a618afade8094fdbad3d95`, { mode: 'cors' })
+        let response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=9fb86b3bf0a618afade8094fdbad3d95`, { mode: 'cors' })
         if(!response.ok){
             throw(await response.json())
         }
@@ -17,7 +17,7 @@ export async function queryClimate(location) {
 }
 export async function queryForecast(location){
     try {
-        let response = await fetch(`http://api.openweathermap.org/data/3.0/forecast?q=${location}&appid=9fb86b3bf0a618afade8094fdbad3d95`, { mode: 'cors' })
+        let response = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${location}&APPID=9fb86b3bf0a618afade8094fdbad3d95`, { mode: 'cors' })
         if(!response.ok){
             throw(await response.json())
         }
